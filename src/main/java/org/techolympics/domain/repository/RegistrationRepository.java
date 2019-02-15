d6e415c1020a47cf6153272ebcfe6032bd6be3d3
James Carman <jwcarman@gmail.com>
2019-02-15 07:35:07 -0500

    Adding school-based lookup.

--- src/main/java/org/techolympics/domain/repository/RegistrationRepository.java
+++ src/main/java/org/techolympics/domain/repository/RegistrationRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.techolympics.domain.entity.Registration;
 
 public interface RegistrationRepository extends CrudRepository<Registration,String> {
+    Iterable<Registration> findBySchool(String school);
 }
